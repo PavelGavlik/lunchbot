8d59e7cd01949836e6c596d427d3abe7ff1521aa
Pavel Gavlik <pavel.gavlik@ceai.io>
2019-05-17 16:38:04 +0200

    Add editorconfig

--- src/velryba.ts
+++ src/velryba.ts
@@ -4,14 +4,14 @@ import fetch from 'node-fetch';
 const crawler = () => fetch('https://www.kavarnavelryba.cz/polednimenu/').then((r) => r.text())
 
 const scraper = (data: string) => {
-    const $ = cheerio.load(data);
+  const $ = cheerio.load(data);
 
-    // TODO check date
-    console.log($('.headline__primary').text() + '\n');
-    console.log(':whale: *Kavárna Velryba*\n', $('.menu-list__item').text().replace(/Kč/g, "Kč\n").trimRight());
+  // TODO check date
+  console.log($('.headline__primary').text() + '\n');
+  console.log(':whale: *Kavárna Velryba*\n', $('.menu-list__item').text().replace(/Kč/g, "Kč\n").trimRight());
 
-    // const [soup, ...rest] = $('.menu-list__item').toArray();
-    // console.log(cheerio(soup).text())
+  // const [soup, ...rest] = $('.menu-list__item').toArray();
+  // console.log(cheerio(soup).text())
 }
 
 crawler().then(scraper);
