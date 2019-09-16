8376c8eb56edac064d227ed4406540a68006060a
Pavel Gavlik <pavel.gavlik@ceai.io>
2019-09-16 11:32:33 +0200

    Add Serial burgers

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import { velryba } from './velryba';
 import { zomato } from './zomato';
 
-const zomatoConfig = {
-  apiKey: 'dcd383753278d533660d82e9d010bd49',
+const apiKey = 'dcd383753278d533660d82e9d010bd49';
+const jinaKrajina = zomato({
+  apiKey,
   id: 16506026,
   emoji: 'earth_asia',
   name: 'Jiná krajina'
-};
-Promise.all([zomato(zomatoConfig)/*, velryba()*/].map(promise => promise.catch((error: Error) => error.message)))
+});
+const serialBurgers = zomato({
+  apiKey,
+  id: 18257533,
+  emoji: 'knife',
+  name: 'Serial burgers'
+});
+Promise.all([jinaKrajina, serialBurgers, velryba()].map(promise => promise.catch((error: Error) => error.message)))
   .then((menus) => console.log(menus.join('\n')));
